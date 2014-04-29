3085578d20684a06a326e72c4ca8f50240a07ccb
Javier Garcia <javitid@gmail.com>
2014-04-29 21:27:05 +0200

    Añadida funcion php para enviar formulario utilizando el mailserver

--- php/contact.php
+++ php/contact.php
@@ -7,7 +7,8 @@
     empty($_POST['comentarios'])||
     !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))    
   { 	
-       echo "No se han pasado correctamente los argumentos!"; 	return false;    
+       echo "No se han pasado correctamente los argumentos!"; 	
+	   return false;    
   } 
 	
   $name = $_POST['nombre']; 
@@ -18,11 +19,11 @@
  // create email body and send it	 
  $to = 'javitid@gmail.com'; 
  // put your email 
- $email_subject = "Contact form submitted by:  $name"; 
- $email_body = "Has recibido un nuevo mensaje . \n\n". 				  
-                   " Detalles:\n \nNombre: $name \n ". 				  
-                   "Email: $email_address\n Telefono: $phone \nComentarios: \n $message"; 
- $headers = "From: contacts@myprogrammingblog.com\n"; 
+ $email_subject = "Contacto desde la web"; 
+ $email_body = "Has recibido un nuevo mensaje! \n\n". 				  
+                   "Nombre: $name \n". 				  
+                   "Email: $email_address\nTelefono: $phone \nComentarios: \n  $message"; 
+ $headers = "From: $email_address\n"; 
  $headers .= "Reply-To: $email_address";	 
 
  mail($to,$email_subject,$email_body,$headers); 
